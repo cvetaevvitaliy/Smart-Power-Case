4f67eb44d61bd7a028076fa352c9fb429947c587
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2020-05-11 17:58:29 +0300

    change type MCU

---
+++ Main_App/HDQ.c
@@ -0,0 +1,5 @@
+//
+// Created by Vitaliy on 5/5/20.
+//
+
+#include "HDQ.h"
---
+++ Main_App/HDQ.h
@@ -0,0 +1,8 @@
+//
+// Created by Vitaliy on 5/5/20.
+//
+
+#ifndef FATSHARK_POWER_V1_HDQ_H
+#define FATSHARK_POWER_V1_HDQ_H
+
+#endif //FATSHARK_POWER_V1_HDQ_H
