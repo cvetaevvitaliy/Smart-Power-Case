5a90019aca5ce62839746302491a30109b0346d5
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2020-06-04 19:44:25 +0300

    add debug interface: use printf on USB, add version FW, debug bq27441

---
+++ Main_App/Debug.c
@@ -0,0 +1,5 @@
+//
+// Created by Vitaliy on 6/4/20.
+//
+
+#include "Debug.h"
---
+++ Main_App/Debug.h
@@ -0,0 +1,8 @@
+//
+// Created by Vitaliy on 6/4/20.
+//
+
+#ifndef FATSHARK_POWER_V1_DEBUG_H
+#define FATSHARK_POWER_V1_DEBUG_H
+
+#endif //FATSHARK_POWER_V1_DEBUG_H
