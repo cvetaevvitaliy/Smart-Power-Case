2474954c1369fd5cf85674b72bbd954a0a251b71
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2020-08-31 16:04:49 +0300

    refactor code

--- Main_App/BQ27441.h
+++ Main_App/BQ27441.h
@@ -63,7 +63,7 @@ bool BQ27441_setCapacity (uint16_t capacity);
 bool BQ27441_setHibernateCurrent(uint16_t current_mA);
 bool BQ27441_setDesignEnergy (uint16_t energy);
 bool BQ27441_setTerminateVoltageMin (uint16_t voltage);
-bool BQ27441_setChargeTermination(uint16_t voltage);
+bool BQ27441_setChargeVChgTermination(uint16_t voltage);
 bool BQ27441_setTaperRateTime (uint16_t rate);
 bool BQ27441_setTaperRateVoltage(uint16_t voltage);
 
