b9bc6f4f87d278d2444a0fe7ac681a74f979b4d5
Vitaliy <vnimych@cybervisiontech.com>
2020-11-14 15:09:34 +0200

    refactor project folder

--- src/device/main_app/CLI_comands.h
+++
@@ -1,8 +0,0 @@
-#ifndef FATSHARK_POWER_V1_CLI_COMANDS_H
-#define FATSHARK_POWER_V1_CLI_COMANDS_H
-#include "cli_config.h"
-#include "main_app.h"
-
-void CLI_cmd_init(void);
-
-#endif //FATSHARK_POWER_V1_CLI_COMANDS_H
